30a2af93410035be8d03eb9d692551c2f5237589
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-10-13 00:00:32 -0400

    Code addition

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
@@ -54,10 +55,11 @@ const App = () => {
         setInfoAlert={setInfoAlert}
       />
 
-      <CityEventsChart
-        allLocations={allLocations} events={events}
-      />
-      
+      <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
+
       <EventList
         events={events}
       />
